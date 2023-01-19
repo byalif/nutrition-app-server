95ca9d462d20e8b4631c2823e7150b3faf25b520
byalif <alif.rahi68@qmail.cuny.edu>
2023-01-18 22:11:19 -0500

    make it better

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 
     app.Use(cors.New(cors.Config{
            AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-        AllowOrigins:     "https://cal-tracker.herokuapp.com",
+        AllowOrigins:     "*",
         AllowCredentials: true,
         AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
     }))
