36a2bd474710c6196b39a7e66a43be12c18e6af3
byalif <alif.rahi68@qmail.cuny.edu>
2023-01-18 21:52:17 -0500

    make it better

--- config/connection.go
+++ config/connection.go
@@ -8,13 +8,10 @@ import (
 )
 
 var  DB *gorm.DB
-
-//mysql://bb78eb07479e92:b8dd168b@us-cdbr-east-06.cleardb.net/heroku_0d3f168b737c65d?reconnect=true
-
 func Connect(){
     log.Println("Connection established")
    
-    connection, err := gorm.Open(mysql.Open("bb78eb07479e92:b8dd168b@tcp(us-cdbr-east-06.cleardb.net:3306)/heroku_0d3f168b737c65d"), &gorm.Config{})
+    connection, err := gorm.Open(mysql.Open("bb78eb07479e92:b8dd168b@tcp(us-cdbr-east-06.cleardb.net:3306)/heroku_0d3f168b737c65d?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
     if err!= nil {
         panic(err)
     }
