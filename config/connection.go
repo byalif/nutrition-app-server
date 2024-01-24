4ff5ba2d2819e500db0dd86e7863b90dfad99f8b
byalif <alif.rahi68@qmail.cuny.edu>
2024-01-24 06:17:23 -0500

    newfiles

--- config/connection.go
+++ config/connection.go
@@ -11,11 +11,11 @@ var  DB *gorm.DB
 func Connect(){
     log.Println("Connection established")
    
-    connection, err := gorm.Open(mysql.Open("bb78eb07479e92:b8dd168b@tcp(us-cdbr-east-06.cleardb.net:3306)/heroku_0d3f168b737c65d?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+    connection, err := gorm.Open(mysql.Open("b10fa4c4417251:a03cd5c6@tcp(us-cluster-east-01.k8s.cleardb.net:3306)/heroku_9e0a4cb90cd7679?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
     if err!= nil {
         panic(err)
     }
-
+    
     DB = connection
 
     connection.AutoMigrate(&models.User{})
